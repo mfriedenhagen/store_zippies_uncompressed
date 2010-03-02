d33bb6b61b0ebc5818bb2c03d00b903af72b2ce3
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2010-03-02 22:22:56 +0100

    Only write tmp xmind for xmind files with thumb nails

--- store_xmind_uncompressed.py
+++ store_xmind_uncompressed.py
@@ -2,28 +2,27 @@ import os
 import sys
 import zipfile
 
-def replaceOriginalWithUncompressedWithoutThumb(name):
+def replaceOriginalWithUncompressedWithoutThumb(name, thumbnailName = "Thumbnails/thumbnail.jpg"):
     infile = zipfile.ZipFile(name, "r")
     tmpname = name + ".tmp"
-    hasThumbnail = False
     try:
-        outfile = zipfile.ZipFile(tmpname, "w", compression=zipfile.ZIP_STORED)
-        try:
-            for f in infile.infolist():
-                if f.orig_filename != "Thumbnails/thumbnail.jpg":
-                    outfile.writestr(f.orig_filename, infile.read(f.orig_filename))
-                else:
-                    hasThumbnail = True
-        finally:
-            outfile.close()
+        hasThumbnail = len([f for f in infile.infolist() if f.orig_filename == thumbnailName]) > 0
+        if hasThumbnail:
+            outfile = zipfile.ZipFile(tmpname, "w", compression=zipfile.ZIP_STORED)
+            try:
+                for f in infile.infolist():
+                    if f.orig_filename != thumbnailName:
+                        outfile.writestr(f.orig_filename, infile.read(f.orig_filename))
+                    else:
+                        hasThumbnail = True
+            finally:
+                outfile.close()
+            print("With thumbnail %s" % name)
+            os.rename(tmpname, name)
+        else:
+            print("Without thumbnail %s" % name)
     finally:
         infile.close()
-    if hasThumbnail:
-        print("With thumbnail %s" % name)
-        os.rename(tmpname, name)
-    else:
-        print("Without thumbnail %s" % name)
-        os.unlink(tmpname)
 
 def addXmind(arg, dirname, fnames):
     for fname in fnames:
@@ -32,6 +31,5 @@ def addXmind(arg, dirname, fnames):
 xminds = []
 os.path.walk(sys.argv[1], addXmind, xminds)
 for xmind in xminds:
-    print xmind
     replaceOriginalWithUncompressedWithoutThumb(xmind)
 
